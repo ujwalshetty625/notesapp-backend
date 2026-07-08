f3ea9d0ded850974922888f1d16b2189c5f3dc52
ujwalshetty625 <ujwalshetty497@gmail.com>
2026-07-08 12:03:23 +0530

    CORS Support added

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://notesapp-frontend-gamma.vercel.app",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-    ],
+    allow_origins = [
+    "https://lemme-take-some-notes.vercel.app",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
